b13ef0de28d9179b0931b4a0013c5a89c7b86f62
robertdabney <robadab02@gmail.com>
2024-04-10 12:42:05 +0800

    parsed lidar

--- src/maze/maze/lidar2distance.py
+++ src/maze/maze/lidar2distance.py
@@ -12,18 +12,25 @@ class ReadingLaser(Node):
 
         self.subscription= self.create_subscription(
             sensor_msgs.msg.LaserScan,
-            '/scan'
-            self.listener_callback
+            '/scan',
+            self.listener_callback,
+            10
         )
 
 
     def listener_callback(self, msg):
-        self.get_logger().info(str(msg))
-
+        num_angles = len(msg.ranges) # 1080
+        # TODO: Figure out which directions are which
+        dir1 = msg.ranges[0]
+        dir2 = msg.ranges[270]
+        dir3 = msg.ranges[540]
+        dir4 = msg.ranges[810]
+        dir5 = msg.ranges[1079]
+        self.get_logger().info(str((dir1,dir2,dir3,dir4,dir5)))
 
 def main(args=None):
     rclpy.init()
-    reading_laser = ReadingLaser()                  
+    reading_laser = ReadingLaser()               
     reading_laser.get_logger().info("Hello friend!")
     rclpy.spin(reading_laser)
 
--- src/maze/setup.py
+++ src/maze/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        	'lidar2dist = maze.lidar2distance:main',
         ],
     },
 )
